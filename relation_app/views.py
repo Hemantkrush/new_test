1c463514c2cabde81be01258e4cca6fca8c5cfca
Hemantkrush <hkarne1239@gmail.com>
2022-11-15 16:12:57 +0530

    created login function

--- relation_app/views.py
+++ relation_app/views.py
@@ -8,5 +8,8 @@ from django.http import HttpResponse, HttpRequest
 def homepage(request):
     return render(request, "homepage.html")
 
+def login(request):
+    return HttpResponse("logged in")
+
 
 
